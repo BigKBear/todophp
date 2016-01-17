a3b730f49d6dfdcd3bfc35b482b1ac2140fa4c62
bigkbear <kylesthill77@gmail.com>
2016-01-17 20:14:19 +0000

    fix index redirect

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
     require 'Service/CookieService.php';
     
-    include 'Views/header.php';
-    
     if (!(isset($_COOKIE['login']) && $_COOKIE['login'] != '')) {
         header("Location: Views/login.php");
     }
